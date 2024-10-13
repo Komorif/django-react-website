41f3e3455c2b9b7c2499a971bf0c3a870ba23501
komori <ererdas@mail.ru>
2024-10-13 18:48:03 +0300

    add polls

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-0ru7-^6yc78rqqlxl83$*^68509t*-&nr-h7yrq2j=73nuy&hk
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
